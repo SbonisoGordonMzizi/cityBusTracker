43948e2846f0d755605fa0f75bdc9b48cd90528b
SbonisoGordonMzizi <gordonmzizi@gmail.com>
2021-05-26 01:07:26 +0200

    adding multiple buses markers on map

--- consumer/app.py
+++ consumer/app.py
@@ -11,14 +11,33 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/topic/<topicname>')
-def get_messages(topicname):
+#handling cityLineBuses messages
+@app.route('/topic/cityLineBuses')
+def get_messages_city():
     client = get_kafka_client()
     def events():
-        for message in client.topics[topicname].get_simple_consumer():
+        for message in client.topics['cityLineBuses'].get_simple_consumer():
                 yield 'data:{0}\n\n'.format(message.value.decode())
     return Response(events(), mimetype="text/event-stream")
-    
+
+
+#handle circleLineBuses messages
+@app.route('/topic/circleLineBuses')
+def get_messages_circle():
+    client = get_kafka_client()
+    def events():
+        for message in client.topics['circleLineBuses'].get_simple_consumer():
+                yield 'data:{0}\n\n'.format(message.value.decode())
+    return Response(events(), mimetype="text/event-stream")
+
+#handle beachLineBuses messages
+@app.route('/topic/beachLineBuses')
+def get_messages_beach():
+    client = get_kafka_client()
+    def events():
+        for message in client.topics['beachLineBuses'].get_simple_consumer():
+                yield 'data:{0}\n\n'.format(message.value.decode())
+    return Response(events(), mimetype="text/event-stream")
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
